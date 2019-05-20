d96cd50dd82bfbe05411ae613989ce39c9a1a87b
gustavjunedahl <gustav.junedahl@gmail.com>
2019-05-20 22:57:03 +0200

    checking storage before forcing sign-in

--- src/app/services/authGuard/auth-guard.guard.ts
+++ src/app/services/authGuard/auth-guard.guard.ts
@@ -1,27 +1,30 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router, CanActivate } from '@angular/router';
 import { Observable } from 'rxjs';
-import { AuthService } from '../auth/auth.service'; 
+import { AuthService } from '../auth/auth.service';
+import { Storage } from '@ionic/storage';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate  {
+export class AuthGuard implements CanActivate {
 
-  constructor(private router: Router, 
-    private auth: AuthService) { }
+  constructor(private router: Router,
+    private auth: AuthService,
+    private storage: Storage) { }
 
   canActivate(route: ActivatedRouteSnapshot): boolean {
+    if (!this.auth.user && !this.checkStorageForUser()) {
+      this.router.navigate(['login']);
+      return false;
+    }
+    return true;
+  }
 
-      console.log(route);
-
-      if (!this.auth.user) {
-          this.router.navigate(['login']);
-          return false;
-      }
-
-      return true;
-
+  async checkStorageForUser() {
+    await this.storage.get('user').then(user => this.auth.user = user);
+    console.log(this.auth.user);
+    return (this.auth.user);
   }
 
 }
